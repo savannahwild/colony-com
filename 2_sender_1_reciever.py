bb8f8134acae6f3308af4da7f5c24660e71143a7
savannahwild <75368358+savannahwild@users.noreply.github.com>
2021-01-30 15:18:44 +0000

    target, maths
    target as gradient
    poss fixed hill

--- 2_sender_1_reciever.py
+++ 2_sender_1_reciever.py
@@ -13,71 +13,71 @@ import helper_functions as hf
 
 
 def main():
-    ## experimental parameters
-    D = 3E-3        # nutrient diffusion coeff (#mm2/min)
-    rho_n = 0.3     # consumption rate of nutrients by X work out?
-    rc = 1E-2          #dhange growth rate of X (on n?) 0.0148?
-    Dc = 1E-5       # cell diffusion coefficient ratev 1.333 go backwards?
-    w = 1           #time
-    Da = 0.0294       #ahl diff constant
-    rho_A = 0.01       #production rate of AHL
-    Dt = 0.03     #random difusion constant of target molecule?
+    
+## experimental parameters
+    D = 3E-3        # nutrient diffusion coeff (#mm2/min) maybe?
+    rho_n = 0.3     #consumption rate of nutrients by X calc?
+    rc = 1E-2     # growth rate of X divisions is max 0.0352
+    Dc = 1E-5       # cell diffusion coefficient? calc that 0.03
+    w = 1           #w = time?
+    Da = 0.0294     #mm2/min
+    rho_A = 0.01    # production rate of AHL
+    Dt = 1e-5       #diffusion rate of target?
 
     environment_size = (60, 60)
     plate = Plate(environment_size)
 
-    ## add nutrient to the plate
+## add nutrient to the plate
     U_N = np.ones(environment_size)
     N = Species("N", U_N)
     def N_behaviour(species, params):
         ## unpack params
         D, rho_n, Dc, rc, w, rho_A, Da, Dt = params
-        n = D * hf.ficks(species['N'], w) - rho_n * species['N'] * (species['M'] + species['R'])
+        n = D * hf.ficks(species['N'], w) - (species['R']+species['M']) * rho_n * hf.leaky_hill(s=species['N'], K=0.15, lam=1, max=rc, min=0) #rho_n * species['N'] * (species['R'])
         return n
     N.set_behaviour(N_behaviour)
     plate.add_species(N)
-    
-    ## add uninformed strain to the plate
+
+## add uninformed strain to the plate
     U_M = np.zeros(environment_size)
-    #U_M[15,30] = 0.001
-    for i in np.linspace(0, 59, environment_size[0]):
-        if (i == 30):
-            U_M[15, int(i)] = 0.001
+    for i in np.linspace(30, 30, 1):
+        for j in np.linspace(30,30, 1):
+            U_M[int(i), int(j)] = 0.001
     M = Species("M", U_M)
     def M_behaviour(species, params):
         ## unpack params
         D, rho_n, Dc, rc, w, rho_A, Da, Dt = params
-        r = Dc * hf.leaky_hill(s=species['A'], K=1, lam=2, max=1e2, min=1) * hf.ficks(species['M'], w) + rc * species['N'] * species['M']
-        #m = Dc * hf.leaky_hill(s=species['A'], K=1, lam=2, max=1e2, min=1) * hf.ficks(species['M'], w) + rc * species['N'] * species['M']
-        #r = Dc * hf.ficks(species['R'], w) + rc * species['N'] * species['R']
-        #########
-        return r
+        m = Dc * hf.leaky_hill(s=species['A'], K=1, lam=2, max=1e2, min=1) * hf.ficks(species['M'], w) + species['M'] * hf.leaky_hill(s=species['N'], K=0.15, lam = 1, max=rc, min=0)
+        #m = Dc * hf.ficks(species['R'], w) + rc * species['N'] * species['M']
+        return m
     M.set_behaviour(M_behaviour)
     plate.add_species(M)
 
-    ## add receiver strain to the plate
+## add receiver strain to the plate
     U_R = np.zeros(environment_size)
-    for i in np.linspace(0, 59, environment_size[0]):
-        if (i == 0) or (i == 59):
-            U_R[15, int(i)] = 0.001
+    for i in np.linspace(30, 30, 1):
+        for j in np.linspace(10,50, 2):
+            U_R[int(i), int(j)] = 0.001
     R = Species("R", U_R)
     def R_behaviour(species, params):
         ## unpack params
         D, rho_n, Dc, rc, w, rho_A, Da, Dt = params
-        
-        r = Dc * hf.leaky_hill(s=species['T'], K=1, lam=2, max=1e2, min=1) * hf.ficks(species['R'], w) + rc * species['N'] * species['R']
-        #r = Dc * hf.ficks(species['R'], w) + rc * species['N'] * species['R']
-        ######### target need induce less motility than ahl!!!
+        r = Dc * hf.leaky_hill(s=species['T'], K=0.85, lam=2, max=1e2, min=1) * hf.ficks(species['R'], w) + species['R'] * hf.leaky_hill(s=species['N'], K=0.15, lam = 1, max=rc, min=0)
+        #r = Dc * hf.leaky_hill(s=species['T'], K=1, lam=2, max=1e2, min=1) * hf.ficks(species['R'], w) + rc * species['N'] * species['R']
         return r
     R.set_behaviour(R_behaviour)
     plate.add_species(R)
 
-    ## add target to plate
+## add target to plate
     U_T = np.zeros(environment_size)
-    for i in np.linspace(0, 59, environment_size[0]):
-        if (i == 30):
-            U_T[15, int(i)] = 0.001
-
+    for i in np.linspace(30, 58, 29):
+        for j in np.linspace(0,59, environment_size[0]):
+            U_T[int(i), int(j)] = ((int(i))-30)/28
+            U_T[59,int(j)]=1E5
+            #where U_T[59,int(j)]=1E5 very high ie membrane
+    #for i in np.linspace(45, 59, 15):
+        #for j in np.linspace(0,59, environment_size[0]):
+            #U_T[int(i), int(j)] = 1
     T = Species("T", U_T)
     def T_behaviour(species, params):
         ## unpack params
@@ -87,19 +87,15 @@ def main():
         return t
     T.set_behaviour(T_behaviour)
     plate.add_species(T)
-
-
-    ## add AHL to plate
+    ##target need induce more motility than ahl? different k 2/3rd than ahl
+    
+## add AHL to plate
     U_A = np.zeros(environment_size)
-    #grad_values = np.logspace(-4, 5, environment_size[0])
-    #for idx, value in enumerate(grad_values):
-        #U_A[idx,:] = value
     A = Species("A", U_A)
     def A_behaviour(species, params):
         ## unpack params
         D, rho_n, Dc, rc, w, rho_A, Da, Dt = params
-        #D, rho_n, Dc, rc, w, rho_A, Da = params
-        a = Da * hf.ficks(species['A'], w) + rho_A * species['R']
+        a = Da * hf.ficks(species['A'], w) + hf.leaky_hill(s=species['R'], K=1, lam=2, max=rho_A, min=0)
         #a = Da * hf.ficks(species['A'], w) + rho_A * species['R']
         #a = 0
         return a
@@ -109,14 +105,14 @@ def main():
     #plate.plot_plate()
     #print("plotted first figure")
 
-    ## run the experiment
+## run the experiment
     params = (D, rho_n, Dc, rc, w, rho_A, Da, Dt)
     sim = plate.run(t_final = 150*60,
                     dt = 2.,
                     params = params)
     print("run simulation")
 
-    ## plotting
+## plotting
     plate.plot_simulation(sim, 10)
 
     print("final plot")
--- 3_sender.py
+++ 3_sender.py
@@ -12,60 +12,52 @@ import helper_functions as hf
 
 
 def main():
-    ## experimental parameters
+## experimental parameters
     D = 3E-3        # nutrient diffusion coeff (#mm2/min) maybe?
     rho_n = 0.3     #consumption rate of nutrients by X calc?
-    rc = 3.5E-2       # growth rate of X divisions is max 0.0352
+    rc = 1E-2     # growth rate of X divisions is max 0.0352
     Dc = 1E-5       # cell diffusion coefficient? calc that 0.03
-    w = 1
-    Da = 0.0294 #mm2/min
-    rho_A = 0.01       # production rate of AHL
-    Dt = 1e-5     #diffusion rate of target?
+    w = 1           #w = time?
+    Da = 0.0294     #mm2/min
+    rho_A = 0.01    # production rate of AHL
+    Dt = 1e-5       #diffusion rate of target?
 
     environment_size = (60, 60)
     plate = Plate(environment_size)
 
-    ## add nutrient to the plate
+## add nutrient to the plate
     U_N = np.ones(environment_size)
     N = Species("N", U_N)
     def N_behaviour(species, params):
         ## unpack params
         D, rho_n, Dc, rc, w, rho_A, Da, Dt = params
-        n = D * hf.ficks(species['N'], w) - species['R'] * hf.leaky_hill(s=species['N'], K=0.15, lam=1, max=rho_n, min=0) #rho_n * species['N'] * (species['R'])
+        n = D * hf.ficks(species['N'], w) - species['R'] * rho_n * hf.leaky_hill(s=species['N'], K=0.15, lam=1, max=rc, min=0) #rho_n * species['N'] * (species['R'])
         return n
     N.set_behaviour(N_behaviour)
     plate.add_species(N)
 
-    ## add receiver strain to the plate
+## add receiver strain to the plate
     U_R = np.zeros(environment_size)
     for i in np.linspace(30, 30, 1):
-        for j in np.linspace(9,51, 3):
-        #for j in np.linspace(0,59, environment_size[0]):
-            #if (4 <= j <= 8) or (16 <= j <= 20) or (28 <= j <= 32) or (40 <= j <= 44) or (52 <= j <= 56):
+        for j in np.linspace(10,50, 3):
             U_R[int(i), int(j)] = 0.001
-
     R = Species("R", U_R)
     def R_behaviour(species, params):
         ## unpack params
         D, rho_n, Dc, rc, w, rho_A, Da, Dt = params
-        
-        r = Dc * hf.leaky_hill(s=species['T'], K=4, lam=2, max=1e2, min=1) * hf.ficks(species['R'], w) + species['R'] * hf.leaky_hill(s=species['N'], K=0.15, lam = 1, max=rc, min=0)
+        r = Dc * hf.leaky_hill(s=species['T'], K=0.1, lam=2, max=1e2, min=1) * hf.ficks(species['R'], w) + species['R'] * hf.leaky_hill(s=species['N'], K=0.15, lam = 1, max=rc, min=0)
         #r = Dc * hf.ficks(species['R'], w) + rc * species['N'] * species['R']
-        #########
         return r
     R.set_behaviour(R_behaviour)
     plate.add_species(R)
 
-    ## add target to plate
+## add target to plate
     U_T = np.zeros(environment_size)
-    for i in np.linspace(50, 59, 10):
+    for i in np.linspace(30, 58, 29):
         for j in np.linspace(0,59, environment_size[0]):
-            U_T[int(i), int(j)] = 100
-        #for j in np.linspace(0,10, environment_size[0]):
-         #   U_T[int(i), int(j)] = 0.1
-        #for j in np.linspace(49,59, environment_size[0]):
-         #   U_T[int(i), int(j)] = 0.1
-
+            U_T[int(i), int(j)] = ((int(i))-30)/28
+            U_T[59,int(j)]=1E5
+            #where U_T[59,int(j)]=1E5 very high ie membrane
     T = Species("T", U_T)
     def T_behaviour(species, params):
         ## unpack params
@@ -76,20 +68,14 @@ def main():
     T.set_behaviour(T_behaviour)
     plate.add_species(T)
 
-    ## add AHL to plate
+## add AHL to plate
     U_A = np.zeros(environment_size)
-    #grad_values = np.logspace(-4, 5, environment_size[0])
-    #for idx, value in enumerate(grad_values):
-        #U_A[idx,:] = value
     A = Species("A", U_A)
     def A_behaviour(species, params):
         ## unpack params
         D, rho_n, Dc, rc, w, rho_A, Da, Dt = params
-        #D, rho_n, Dc, rc, w, rho_A, Da = params
-        #a = Da * hf.ficks(species['A'], w)
         a = Da * hf.ficks(species['A'], w) + hf.leaky_hill(s=species['R'], K=1e-4, lam=2, max=rho_A, min=0) #rho_n * species['N'] * (species['R'])#rho_A * species['R']
         #a = 0
-        #w = time?
         return a
     A.set_behaviour(A_behaviour)
     plate.add_species(A)
@@ -97,16 +83,15 @@ def main():
     #plate.plot_plate()
     #print("plotted first figure")
 
-    ## run the experiment
+## run the experiment
     params = (D, rho_n, Dc, rc, w, rho_A, Da, Dt)
     sim = plate.run(t_final = 150*60,
                     dt = 2.,
                     params = params)
     print("run simulation")
 
-    ## plotting
+## plotting
     plate.plot_simulation(sim, 10)
-
     print("final plot")
 
 main()
